9f42019425aecf36ae9c80e5778d8f70df08c49c
Callum Macmillan <Callum@Lilys-MacBook-Air.local>
2022-10-01 10:26:28 +0100

    Added bit of map loader

--- site/src/client/GameScript.ts
+++ site/src/client/GameScript.ts
@@ -3,6 +3,7 @@ import { BallEntity } from "./engine/entities/BallEntity";
 import { MinerEntity } from "./engine/entities/MinerEntity";
 import { Game } from "./Game";
 import { GameMap } from "./Map";
+import { MapLoader } from "./MapLoader";
 import { InputState } from "./services/input/InputService";
 import { ServiceLocator } from "./services/ServiceLocator";
 import { Camera } from "./types";
@@ -25,8 +26,11 @@ export class GameScript {
 
     public newGame() {
         // Create and add the map to the world
-        this.gameMap = new GameMap(this.serviceLocator);
-        this.serviceLocator.getWorld().addEntity(this.gameMap);
+        const mapLoader = new MapLoader(this.serviceLocator);
+        const entities = mapLoader.getEntities();
+        for (let entity of entities) {
+            this.serviceLocator.getWorld().addEntity(entity);
+        }
 
         for (let i = 0; i < 10; i++) {
             const x = randomIntRange(0, WIDTH);
